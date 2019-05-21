ab2aa495117abf9e33c95855180ee78ba63e655b
benbanuz <benbanuz@users.noreply.github.com>
2019-05-21 18:49:58 +0300

    choose best model with k-cross

--- wrapers.py
+++ wrapers.py
@@ -42,3 +42,28 @@ def split_lab_exam(df: pd.DataFrame):
     labels = df["Vote"]
     exampels = df[featuers]
     return labels, exampels
+
+
+def cross_valid(model, validation_set: pd.DataFrame, num_folds: int):
+    kf = KFold(n_splits=num_folds)
+    acc = 0
+    for train_indexes, test_indexes in kf.split(validation_set):
+        train_set = validation_set[train_indexes]
+        test_set = validation_set[test_indexes]
+        train_labels, train_exampels = split_lab_exam(train_set)
+        test_labels, test_exampels = split_lab_exam(test_set)
+        model.fit(train_exampels, train_labels)
+        pred = model.predict(test_exampels)
+        acc += sum(pred == test_labels) / len(test_indexes)
+    return acc / num_folds
+
+
+def choose_best_model(models, df: pd.DataFrame, num_folds: int):
+    best_acc = 0
+    best_model = models[0]
+    for model in models:
+        acc = cross_valid(model, df, num_folds)
+        if acc > best_acc:
+            best_acc = acc
+            best_model = model
+    return best_model

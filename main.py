04812947b369a5e3d9dd82f2b17966fab34178fd
benbanuz <benbanuz@users.noreply.github.com>
2019-05-21 17:12:29 +0300

    majority by proba

--- main.py
+++ main.py
@@ -1,30 +1,14 @@
 import loading
 import numpy as np
 import pandas as pd
-import sklearn
+import sklearn as sk
 import matplotlib.pyplot as plt
 from imputation import *
 import cleansing
-from feat_selection import *
 from standartisation import *
 from sklearn.tree import DecisionTreeClassifier
 from pprint import pprint
-from relief_algo import relief
-
-
-def show_info(df: pd.DataFrame, stage: str):
-    plt.imshow(df.corr().abs())
-    plt.colorbar()
-    plt.title(f'correlation after {stage}')
-    plt.show()
-    plt.close('all')
-
-    plt.imshow(calc_MI_matrix(df))
-    plt.colorbar()
-    plt.title(f'MI after {stage}')
-    plt.show()
-    plt.close('all')
-
+import wrapers
 
 def rows_in_df(df: pd.DataFrame, rows: List[int]) -> List[int]:
     return list(set(df.index.values.tolist()).intersection(set(rows)))
@@ -55,67 +39,8 @@ def main():
     imputation(test, train)
     test.to_csv("test_after_imputation.csv", index=False)
 
-    # train = pd.read_csv('train_after_imputation.csv')
-    # valid = pd.read_csv('valid_after_imputation.csv')
 
-    show_info(train, "imputation")
 
-    features: List[str] = train.columns.to_numpy().tolist()
-    non_norm_feats = ["Looking_at_poles_results", "Married", "Will_vote_only_large_party", "Financial_agenda_matters"]
-    non_norm_feats += [f for f in features if f.startswith('Occ') or f.startswith('trans') or f.startswith("Issue")]
-
-    scaler = DFScaler(train, list(set(features).difference(set(non_norm_feats + ['Vote']))))
-
-    features_close = remove_similar_features(train, 'Vote', 8)
-    print(f'features after close removal:')
-    pprint(features_close)
-    show_info(train[features_close], "close removal")
-
-    # features_far = remove_far_features(train, 'Vote', 8)
-    # print(f'features after far removal:')
-    # pprint(features_far)
-    # show_info(train[features_far], "far removal")
-
-    features_close_far = remove_similar_features(train[features_close], 'Vote', 8)
-    print(f'features after close and then far removal:')
-    pprint(features_close_far)
-    show_info(train[features_close_far], "close and then far removal")
-
-    # features_far_close = remove_far_features(train[features_far], 'Vote', 8)
-    # print(f'features after far and then close removal:')
-    # pprint(features_far_close)
-    # show_info(train[features_far_close], "far and then close removal")
-
-    train = scaler.scale(train)
-    valid = scaler.scale(valid)
-    test = scaler.scale(test)
-    show_info(train, "scaling")
-
-    relief_features = relief(train[features_close_far], 'Vote', 8)
-    relief_features += ['Vote']
-    print(f'features after relief:')
-    pprint(relief_features)
-    show_info(train[relief_features], "relief")
-
-    sfs_features = wrapper_SFS(train[relief_features], valid[relief_features], 'Vote', DecisionTreeClassifier())
-    sfs_features += ['Vote']
-    print(f'features after SFS:')
-    pprint(sfs_features)
-    show_info(train[sfs_features], "SFS")
-
-    sbs_features = wrapper_SBS(train[relief_features], valid[relief_features], 'Vote', DecisionTreeClassifier())
-    sbs_features += ['Vote']
-    print(f'features after SBS:')
-    pprint(sbs_features)
-    show_info(train[sbs_features], "SBS")
-
-    # TODO choose final features
-    final_features = sbs_features
-    print("the final features are:")
-    pprint(final_features)
-    train[final_features].to_csv('train_final.csv', index=False)
-    valid[final_features].to_csv('valid_final.csv', index=False)
-    test[final_features].to_csv('test_final.csv', index=False)
 
 
 if __name__ == "__main__":
--- wrapers.py
+++ wrapers.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
 import sklearn as sk
+from sklearn.model_selection import KFold
 
 
-class wraper:
+class major_wraper:
     def __init__(self, model):
         self.model = model
 
@@ -11,7 +12,22 @@ class wraper:
         self.model.fit(x_train_set, y_train_set)
 
     def pred_label(self, pred_set: pd.DataFrame):
-        prediction = self.model.pred(pred_set)
         probs_predictions = self.model.predict_proba(pred_set)
         results = np.mean(probs_predictions, axis=0)
         return np.argmax(results)
+
+
+class pick_model:
+    def __init__(self, models):
+        self.models = models
+
+    def fit_train(self, x_train_set: pd.DataFrame, y_train_set: pd.DataFrame):
+        for model in self.models:
+            model.fit(x_train_set, y_train_set)
+
+    def choose_best(self, validation_set: pd.DataFrame,num_folds):
+        best_acc = 0
+        best_model = self.models[0]
+        kf = KFold(n_splits=num_folds)
+        for model in self.models:
+            acc =

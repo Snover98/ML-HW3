fde408be053554a012d280b3e7d37a71beb8b1f0
Assaf-Haik-Barouch <Assaf98@gmail.com>
2019-05-22 16:59:54 +0200

    we now look for hyper-parameters on the normal problem

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from data_preperation import prepare_data
 import numpy as np
 from wrappers import *
 from model_selection import *
-from scipy.stats import uniform
-import scipy
+from scipy.stats import uniform, expon
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 
 
 class LogUniform:
@@ -17,31 +19,40 @@ class LogUniform:
         return np.power(self.base, np.random.uniform(self.low, self.high, self.size))
 
 
+def wrapper_params(params: dict, to_add: dict = {}):
+    new_params = {'model__' + key: item for key, item in params.items()}
+    new_params.update(to_add)
+    return new_params
+
+
 def main():
     train, valid, test = prepare_data()
+    features = list(set(train.to_numpy().tolist()).difference({'Vote'}))
 
     random_forest_params = {
         'n_estimators': [10, 20, 50, 80, 100, 120],
         'criterion': ['gini', 'entropy'],
         'min_samples_split': [2, 3, 4, 5, 10, 20],
         'min_samples_leaf': uniform(),
-        'max_features ': ['auto', 'sqrt', 'log2', None],
-        'class_weight': ['balanced']
+        'max_features ': ['auto', 'sqrt', 'log2', None]
     }
 
-    svm_params = {
-        'C': scipy.stats.expon(scale=100),
+    svc_params = {
+        'C': expon(scale=100),
         'kernel': ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed'],
         'degree': [3, 4, 5],
         'gamma': ['scale', 'auto'],
-        'probability': [True],
-        'tol': LogUniform(low=-10, high=0),
-        'class_weight': ['balanced'],
-        'max_iter': [1, 2, 5, 10]
+        'tol': LogUniform(low=-10, high=0)
     }
 
+    svc_normal = SVC(probability=True, class_weight='balanced')
+    rand_forest_normal = RandomForestClassifier(class_weight='balanced')
 
+    svc_grid_normal = RandomizedSearchCV(svc_normal, svc_params, scoring=evaluate_voters_division, cv=3)
+    svc_grid_normal.fit(train[features], train['Vote'])
 
-
+    random_forest_normal = RandomizedSearchCV(rand_forest_normal, random_forest_params,
+                                              scoring=evaluate_voters_division, cv=3)
+    random_forest_normal.fit(train[features], train['Vote'])
 
     pass
--- wrappers.py
+++ wrappers.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
+from sklearn.base import BaseEstimator
 
 
-class ElectionsResultsWrapper:
+class ElectionsResultsWrapper(BaseEstimator):
     def __init__(self, model):
         self.model = model
         self.targets = None
@@ -21,7 +22,7 @@ class ElectionsResultsWrapper:
         return np.sum(probs_predictions, axis=0)
 
 
-class LikelyVotersWrapper:
+class LikelyVotersWrapper(BaseEstimator):
     def __init__(self, model, threshold: float):
         assert 0 < threshold < 1.0
         self.model = model

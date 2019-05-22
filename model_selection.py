24c55e0ab793a0fae267fe521bca684894fd9608
Assaf-Haik-Barouch <Assaf98@gmail.com>
2019-05-22 12:29:08 +0200

    added evaluation functions for all 3 classification problems made evaluation more generic and dependent on the evaluation func

--- model_selection.py
+++ model_selection.py
@@ -1,9 +1,48 @@
 import numpy as np
 import pandas as pd
 import sklearn as sk
-from sklearn.model_selection import KFold
+from sklearn.model_selection import StratifiedKFold
 from sklearn.utils import resample
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelBinarizer
+from numpy.linalg import norm
+
+
+def evaluate_voters_division(estimator, y_true, X) -> float:
+    y_pred = estimator.predict(X)
+    return accuracy_score(y_true, y_pred)
+
+
+def evaluate_election_winner(estimator, y_true, X) -> float:
+    lb = LabelBinarizer()
+    lb.fit(y_true)
+
+    hist_true = np.sum(lb.transform(y_true), axis=0)
+    hist_pred = estimator.predict_proba(X)
+
+    return -norm(hist_true-hist_pred)
+
+
+def evaluate_party_voters(estimator, y_true, X, party):
+    indices_true = y_true.index[y_true == party]
+    indices_pred = estimator.predict(X)
+
+    true_pos_indices = list(set(indices_true).intersection(set(indices_pred)))
+
+    return np.sqrt((len(true_pos_indices) ** 2) / (len(indices_true) * len(indices_pred)))
+
+
+def evaluate_likely_voters(estimator, y_true, X):
+    """
+
+    :param estimator: estimator
+    :param y_true: should be valid['Vote'] or something like that
+    :param X: should be valid[features] or something like that
+    :return: average score for each party
+    """
+    parties = y_true.unique()
+
+    return np.mean([evaluate_party_voters(estimator, y_true, X, party) for party in parties])
 
 
 def upsample(df: pd.DataFrame, target: str) -> pd.DataFrame:
@@ -26,22 +65,24 @@ def target_features_split(df: pd.DataFrame, target: str):
     return df[target], df[features]
 
 
-def cross_valid(model, df: pd.DataFrame, num_folds: int):
-    kf = KFold(n_splits=num_folds)
-    acc = 0
-    for train_indexes, test_indexes in kf.split(df):
-        train_set = df[train_indexes]
-        test_set = df[test_indexes]
-        train_targets, train_features = target_features_split(train_set, 'Vote')
-        test_targets, test_features = target_features_split(test_set, 'Vote')
+def cross_valid(model, df: pd.DataFrame, num_folds: int, eval_func):
+    kf = StratifiedKFold(n_splits=num_folds)
+    score = -np.inf
+
+    df_targets, df_features = target_features_split(df, 'Vote')
+
+    for train_indices, test_indices in kf.split(df_features, df_targets):
+        train_targets, train_features = df_targets[train_indices], df_features[train_indices]
+        test_targets, test_features = df_targets[test_indices], df_features[test_indices]
+
         model.fit(train_features, train_targets)
-        pred = model.predict(test_features)
-        acc += accuracy_score(test_targets, pred)
-    return acc / num_folds
+        score += eval_func(model, test_targets, test_features)
+
+    return score / num_folds
 
 
-def choose_best_model(models, train: pd.DataFrame, valid: pd.DataFrame):
-    best_acc = 0
+def choose_best_model(models, train: pd.DataFrame, valid: pd.DataFrame, eval_func):
+    best_score = -np.inf
     best_model = None
 
     train_targets, train_features = target_features_split(train, 'Vote')
@@ -49,9 +90,10 @@ def choose_best_model(models, train: pd.DataFrame, valid: pd.DataFrame):
     
     for model in models:
         model.fit(train_features, train_targets)
-        pred = model.predict(valid_features)
-        acc = accuracy_score(valid_targets, pred)
-        if acc > best_acc:
-            best_acc = acc
+        score = eval_func(model, valid_targets, valid_features)
+
+        if score > best_score:
+            best_score = score
             best_model = model
+
     return best_model

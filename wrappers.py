13dbae421518459e0db7a18c37530c81100e74be
Assaf-Haik-Barouch <Assaf98@gmail.com>
2019-05-22 09:33:37 +0200

    renamed stuff made outputs make more sense

--- wrappers.py
+++ wrappers.py
@@ -4,34 +4,39 @@ import sklearn as sk
 from sklearn.model_selection import KFold
 
 
-class major_wraper:
+class ElectionsResultsWrapper:
     def __init__(self, model):
         self.model = model
 
-    def fit_train(self, x_train_set: pd.DataFrame, y_train_set: pd.DataFrame):
+    def fit(self, x_train_set: pd.DataFrame, y_train_set: pd.DataFrame):
         self.model.fit(x_train_set, y_train_set)
 
-    def pred_label(self, pred_set: pd.DataFrame):
+    def predict(self, pred_set: pd.DataFrame):
         probs_predictions = self.model.predict_proba(pred_set)
-        results = np.mean(probs_predictions, axis=0)
+        results = np.sum(probs_predictions, axis=0)
         return np.argmax(results)
 
+    def predict_proba(self, pred_set: pd.DataFrame):
+        probs_predictions = self.model.predict_proba(pred_set)
+        return np.sum(probs_predictions, axis=0)
 
-class probably_vote:
-    def __init__(self, model, threshold: int):
+
+class LikelyVotersWrapper:
+    def __init__(self, model, threshold: float):
+        assert 0 < threshold < 1.0
         self.model = model
         self.threshold = threshold
 
-    def fit_train(self, x_train_set: pd.DataFrame, y_train_set: pd.DataFrame):
+    def fit(self, x_train_set: pd.DataFrame, y_train_set: pd.DataFrame):
         self.model.fit(x_train_set, y_train_set)
 
-    def prob_vote(self, df: pd.DataFrame, party: int):
+    def predict(self, df: pd.DataFrame, party: int):
         will_vote = []
         probs_predictions = self.model.predict_proba(df)
         for idx, row in enumerate(probs_predictions):
             if row[party] > self.threshold:
                 will_vote.append(idx)
-        return df.iloc[will_vote, :]
+        return will_vote
 
 
 def split_lab_exam(df: pd.DataFrame):

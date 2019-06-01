33c3c10d2e1491cf6eda9c813134cf36155fb636
Snover98 <Assaf98@Gmail.com>
2019-06-01 15:40:24 +0300

    added confusion matrix plot

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from model_selection import *
 from sklearn.metrics import confusion_matrix
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+from sklearn.utils.multiclass import unique_labels
 from pprint import pprint
 
 
@@ -43,6 +45,59 @@ class RandIntMult:
             state = np.random.RandomState(random_state)
 
         return np.around(state.uniform(low=self.low, high=self.high, size=self.size) * self.mult).astype(int)
+
+
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    plt.show()
+    return ax
 
 
 def find_best_models(train, valid, search_hyper_params=True, verbose=False):
@@ -100,9 +155,11 @@ def use_estimators(best_estimators, train, valid, test):
     test_true = test['Vote']
     parties = non_test_data['Vote'].unique()
     conf_matrix = pd.DataFrame(confusion_matrix(test_true, test_pred, parties), columns=parties, index=parties)
-    print('The confusion matrix is:')
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-        print(conf_matrix)
+    #print('The confusion matrix is:')
+    plot_confusion_matrix(test_true, test_pred, parties)
+    
+    #with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        #print(conf_matrix)
     print('')
     test_pred.to_csv('test_predictions.csv', index=False)
 
